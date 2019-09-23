267fa49c3156cf87a9a80561fc1476f89f23b5d0
maddy13vb <45980321+maddy13vb@users.noreply.github.com>
2019-09-23 17:03:31 -0400

    card created using for loops and arrays

--- blackjack.js
+++ blackjack.js
@@ -1,15 +1,38 @@
-// creating the card variables
-let deck = [
-    "Ace of Spades",
-    "Two of Spades", 
-    "Three of Spades"
-];
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
 
+
+
+// creating the card array
+let deck = [];
+
+for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+        deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+    }
+}
+for (let i = 0; i < deck.length; i++) {
+    console.log(deck[i]);
+}
+let playerCards = [deck[0], deck[2]];
+
+let score = 1000;
+if (score > 1000) {
+    score = score + 100;
+    console.log("Great job!");
+} else if (score === 1000) {
+    console.log("Almost!")
+} else {
+    console.log("No bonus!");
+}
+
+
+console.log("Score is : ", score); //1100
 
 // console logging messages
 console.log("Welcome to Blackjack!");
 console.log("Your cards are:");
-console.log(" " + card1);
-console.log(" " + card2);
+console.log(" " + playerCards[0]);
+console.log(" " + playerCards[1]);
 
 
