0da8b6895aab7eaa81507d485364fc5cfedd5b8d
maddy13vb <45980321+maddy13vb@users.noreply.github.com>
2019-09-23 16:09:27 -0400

    creating variables and console.log

---
+++ blackjack.js
@@ -0,0 +1,15 @@
+// creating the card variables
+let deck = [
+    "Ace of Spades",
+    "Two of Spades", 
+    "Three of Spades"
+];
+
+
+// console logging messages
+console.log("Welcome to Blackjack!");
+console.log("Your cards are:");
+console.log(" " + card1);
+console.log(" " + card2);
+
+
