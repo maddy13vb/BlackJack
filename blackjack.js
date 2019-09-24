75cbe6b8555d94f8a3505aa558b1853c11375cc8
maddy13vb <45980321+maddy13vb@users.noreply.github.com>
2019-09-24 09:40:31 -0400

    creating objects and using js built ins

--- blackjack.js
+++ blackjack.js
@@ -2,19 +2,25 @@ let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
 
 
-
-// creating the card array
-let deck = [];
-
-for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
-    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-        deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+function createDeck() {
+     let deck = [];
+    for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+        for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+            deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+        }
     }
+    return deck;
 }
+
+function getNextCard() {
+    return deck.shift();
+}
+let deck = createDeck();
+
 for (let i = 0; i < deck.length; i++) {
     console.log(deck[i]);
 }
-let playerCards = [deck[0], deck[2]];
+let playerCards = [getNextCard(), getNextCard()]; //gets 2 cards
 
 let score = 1000;
 if (score > 1000) {
@@ -29,10 +35,24 @@ if (score > 1000) {
 
 console.log("Score is : ", score); //1100
 
-// console logging messages
+// console log messages
 console.log("Welcome to Blackjack!");
 console.log("Your cards are:");
 console.log(" " + playerCards[0]);
 console.log(" " + playerCards[1]);
 
+function changeCard(card) {
+    card.suit = "Clubs";
+}
 
+let card = [
+    {
+    suit: "Hearts",
+    value: "Queen"
+    }
+];
+console.log(card[0].suit); // Hearts
+
+//using built ins
+let result = 0/0;
+console.log(Number.isNaN(result)); // true
