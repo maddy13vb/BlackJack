1e86d71d84d79d3340d33423456fae277f224415
maddy13vb <45980321+maddy13vb@users.noreply.github.com>
2019-09-24 13:08:04 -0400

    progress made

--- blackjack.js
+++ blackjack.js
@@ -1,58 +1,102 @@
+
+// card variables
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
+
+// DOM variables
+let paragraph = document.getElementById('text-area'),
+    clickHit = document.getElementById('hit'),
+    stayHere = document.getElementById('stay'),
+    newGame = document.getElementById('new-game');
+
+
+
+// Game variables
+let gameStarted = false,
+    gameOver = false,
+    playerWon = false,
+    dealerCards = [],
+    playerCards = [],
+    dealerScore = 0,
+    playerScore = 0,
+    deck = [];
+
+// initial placement of the buttons
+clickHit.style.display = 'none';
+stayHere.style.display = 'none';
+showStatus();
+
+// click event
+newGame.addEventListener('click', function () {
+    gameStarted = true;
+    gameOver = false;
+    playerWon = false;
+
+    deck = createDeck();
+    shuffleDeck(deck);
+    playerCards = [getNextCard(), getNextCard()]; //gets 2 cards
+    dealerCards = [getNextCard(), getNextCard()];
+
+    newGame.style.display = 'none';
+    clickHit.style.display = 'inline';
+    stayHere.style.display = 'inline';
+    showStatus();
+});
 
 
 function createDeck() {
      let deck = [];
     for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
         for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-            deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+            let card = {
+                suit: suits[suitIdx],
+                value: values[valueIdx]
+            };
+            deck.push(card);
         }
     }
     return deck;
 }
 
+// shuffle deck
+function shuffleDeck(deck) {
+    for (let i = 0; i < deck.length; i++) {
+        let swapIdx = Math.trunc(Math.random() * deck.length);
+        let tmp = deck[swapIdx];
+        deck[swapIdx] = deck[i];
+        deck[i] = tmp;
+    }
+}
+
+function getCardString(card) {
+    return card.value + ' of ' + card.suit;
+}
+
 function getNextCard() {
     return deck.shift();
 }
-let deck = createDeck();
 
-for (let i = 0; i < deck.length; i++) {
-    console.log(deck[i]);
-}
-let playerCards = [getNextCard(), getNextCard()]; //gets 2 cards
-
-let score = 1000;
-if (score > 1000) {
-    score = score + 100;
-    console.log("Great job!");
-} else if (score === 1000) {
-    console.log("Almost!")
-} else {
-    console.log("No bonus!");
-}
-
-
-console.log("Score is : ", score); //1100
-
-// console log messages
-console.log("Welcome to Blackjack!");
-console.log("Your cards are:");
-console.log(" " + playerCards[0]);
-console.log(" " + playerCards[1]);
-
-function changeCard(card) {
-    card.suit = "Clubs";
-}
-
-let card = [
-    {
-    suit: "Hearts",
-    value: "Queen"
+function showStatus() {
+    if (!gameStarted) {
+        paragraph.innerText = 'Welcome to Blackjack!';
+        return;
     }
-];
-console.log(card[0].suit); // Hearts
+}
 
-//using built ins
-let result = 0/0;
-console.log(Number.isNaN(result)); // true
+for (var i = 0; i < deck.length; i++) {
+    paragraph.innerText += '\n' + getCardString(deck[i]); 
+}
+
+
+
+
+// function changeCard(card) {
+//     card.suit = "Clubs";
+// }
+
+// let card = [
+//     {
+//     suit: "Hearts",
+//     value: "Queen"
+//     }
+// ];
